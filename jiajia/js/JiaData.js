7d8ec2dedf17eccac350d1f2c487539a3565e9a6
CrkJK <crkjk@CrkJKde-MacBook-Pro.local>
2018-09-18 10:51:59 +0800

    crkJK
    test

---
+++ jiajia/js/JiaData.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyAUnA_8r0-qHb6bCTMi0SCzHcUlc07Qbdk",
+    authDomain: "jia618-d8042.firebaseapp.com",
+    databaseURL: "https://jia618-d8042.firebaseio.com",
+    projectId: "jia618-d8042",
+    storageBucket: "jia618-d8042.appspot.com",
+    messagingSenderId: "157048895266"
+};
+firebase.initializeApp(config);
